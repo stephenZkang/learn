24c1bc47cc23d7437735a2f38a9b97e3db8414e5
stephenZkang <qiaok9921@163.com>
2020-05-27 09:17:13 +0800

    找重复数，还是一脸懵逼

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice2/FindDuplicate.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice2/FindDuplicate.java
@@ -39,13 +39,83 @@ public class FindDuplicate {
     }
 
     /**
-     *
+     * 二分查找
      * 时间复杂度：
      * 空间复杂度：
      * @param nums
      * @return
      */
     private int findDuplicate(int[] nums) {
-        return 0;
+        int n = nums.length;int ans = -1;
+        int l =0; int r = n-1;
+        while(l<=r){
+            int mid = (l+r) >> 1;
+            int cnt = 0;
+            for (int i = 0; i < n; ++i) {
+                if(nums[i]<= nums[mid]){
+                    cnt++;
+                }
+            }
+            if(cnt<=mid){
+                l = mid+1;
+            }else{
+                r = mid -1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 二进制
+     * 时间复杂度：
+     * 空间复杂度：
+     * @param nums
+     * @return
+     */
+    private int findDuplicate1(int[] nums) {
+        int n = nums.length;int ans = 0;
+        int bit_max = 31;
+        while(((n-1)>>bit_max)==0){
+            bit_max-=1;
+        }
+
+        for (int bit = 0; bit < bit_max; ++bit) {
+            int x = 0;int y = 0;
+            for (int i = 0; i < n; ++i) {
+                if((nums[i]&(1<<bit))!=0){
+                    x+=1;
+                }
+                if(i>=1&&(i&(1<<bit))!=0){
+                    y+=1;
+                }
+            }
+            if(x>y){
+                ans |= 1 << bit;
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * 快慢指针
+     * 时间复杂度：
+     * 空间复杂度：
+     * @param nums
+     * @return
+     */
+    private int findDuplicate2(int[] nums) {
+        int slow = 0; int fast =0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        slow=0;
+        while (slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        return slow;
+
     }
 }
