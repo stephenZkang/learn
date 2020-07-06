13a6545d0f30fd11c71279479e1a127995ac9feb
乔康 <qiaok@superred.com.cn>
2020-07-06 08:58:56 +0800

    63. 不同路径 II

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/FindKthLargest.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/FindKthLargest.java
@@ -1,5 +1,7 @@
 package com.leetcode.qiaok.practice3;
 
+import java.util.Arrays;
+
 /**
  * 215. 数组中的第K个最大元素
  * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
@@ -26,7 +28,7 @@ public class FindKthLargest {
         int[] nums = { 3,2,1,5,6,4 };
         int k = 2;
         long start = System.currentTimeMillis();
-        int res = test.findKthLarget(nums,k);
+        int res = test.findKthLarget1(nums,k);
         System.out.println("耗时"+(System.currentTimeMillis() -start)+"毫秒");
         System.out.println("res="+res);
     }
@@ -40,6 +42,53 @@ public class FindKthLargest {
      * @return
      */
     public int findKthLarget(int[] nums, int k) {
-        return 0;
+        Arrays.sort(nums);
+        return nums[nums.length-k];
+    }
+
+    /**
+     * 基于堆排序的选择方法
+     * 时间复杂度：O(nlogn)
+     * 空间复杂度：O(logn)
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int findKthLarget1(int[] nums, int k) {
+        int heapSize = nums.length;
+        buildMaxHeap(nums, heapSize);
+        for (int i = nums.length - 1; i >= nums.length - k + 1; --i) {
+            swap(nums, 0, i);
+            --heapSize;
+            maxHeapify(nums, 0, heapSize);
+        }
+        return nums[0];
+    }
+
+    private void buildMaxHeap(int[] nums, int heapSize) {
+        for (int i = heapSize / 2; i >= 0; --i) {
+            maxHeapify(nums, i, heapSize);
+        }
+    }
+
+
+    public void maxHeapify(int[] a, int i, int heapSize) {
+        int l = i * 2 + 1, r = i * 2 + 2, largest = i;
+        if (l < heapSize && a[l] > a[largest]) {
+            largest = l;
+        }
+        if (r < heapSize && a[r] > a[largest]) {
+            largest = r;
+        }
+        if (largest != i) {
+            swap(a, i, largest);
+            maxHeapify(a, largest, heapSize);
+        }
+    }
+
+    public void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
