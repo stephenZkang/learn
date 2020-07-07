2a32b0ccdde834823df7360443f6b9e6ef345185
乔康 <qiaok@superred.com.cn>
2020-07-07 08:31:06 +0800

    63. 不同路径 II 二次练习 动态规划-条件判断是难点

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice2/UniquePathsWithObstacles.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice2/UniquePathsWithObstacles.java
@@ -53,13 +53,30 @@ public class UniquePathsWithObstacles {
     }
 
     /**
-     *
+     * 动态规划
      * 时间复杂度：
      * 空间复杂度：
      * @param obstacleGrid
      * @return
      */
-    private int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        return 0;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length;
+        int m = obstacleGrid[0].length;
+        //定义dp
+        int[] f = new int[m];
+        //复制dp[0]
+        f[0] = obstacleGrid[0][0] == 0?1:0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(obstacleGrid[i][j] == 1){
+                    f[j]=0;
+                    continue;
+                }
+                if(j-1>=0&&obstacleGrid[i][j-1]==0){
+                    f[j] +=f[j-1];
+                }
+            }
+        }
+        return f[m-1];
     }
 }
