9b4fd7d55dec293de921cab233264b44ec926355
stephenZkang <qiaok9921@163.com>
2020-07-20 08:52:54 +0800

    167. 两数之和 II - 输入有序数组

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/Intersect.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/Intersect.java
@@ -1,6 +1,8 @@
 package com.leetcode.qiaok.practice3;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 350. 两个数组的交集 II
@@ -38,10 +40,10 @@ public class Intersect {
     public static void main(String[] args){
         Intersect test = new Intersect();
         int[] nums1 = {
-
+                4,9,5
         };
         int[] nums2 = {
-
+                9,4,9,8,4
         };
 
         long start = System.currentTimeMillis();
@@ -60,6 +62,53 @@ public class Intersect {
      * @return
      */
     public int[] intersect(int[] nums1, int[] nums2) {
-        return new int[0];
+        if(nums1.length > nums2.length){
+            intersect(nums2,nums1);
+        }
+
+        Map<Integer,Integer> temp = new HashMap<>();
+        for (int num:nums1) {
+            int count = temp.getOrDefault(num,0)+1;
+            temp.put(num,count);
+        }
+
+        int[] ins = new int[nums1.length];
+        int index =0;
+        for (int num:nums2) {
+            int count = temp.getOrDefault(num, 0);
+            if (count > 0) {
+                ins[index++] = num;
+                count--;
+                if (count > 0) {
+                    temp.put(num, count);
+                } else {
+                    temp.remove(num);
+                }
+            }
+        }
+        return Arrays.copyOfRange(ins, 0, index);
+
+    }
+
+    public int[] intersect1(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int length1 = nums1.length, length2 = nums2.length;
+        int[] intersection = new int[Math.min(length1, length2)];
+        int index1 = 0, index2 = 0, index = 0;
+        while (index1 < length1 && index2 < length2) {
+            if (nums1[index1] < nums2[index2]) {
+                index1++;
+            } else if (nums1[index1] > nums2[index2]) {
+                index2++;
+            } else {
+                intersection[index] = nums1[index1];
+                index1++;
+                index2++;
+                index++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+
     }
 }
