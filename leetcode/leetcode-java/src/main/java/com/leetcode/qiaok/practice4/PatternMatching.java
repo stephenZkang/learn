60884ecba6fa7a7c320e5632cd3e7711559ee5e8
stephenZkang <qiaok9921@163.com>
2020-07-22 08:50:28 +0800

    面试题 17.13. 恢复空格

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice4/PatternMatching.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice4/PatternMatching.java
@@ -39,8 +39,8 @@ public class PatternMatching {
      */
     public static void main(String[] args){
         PatternMatching test = new PatternMatching();
-        String pattern = "";
-        String value = "";
+        String pattern = "aaaa";
+        String value = "dogcatcatdog";
         long start = System.currentTimeMillis();
         boolean res = test.patternMatching(pattern,value);
         System.out.println("耗时"+(System.currentTimeMillis() - start)+"毫秒");
@@ -57,6 +57,67 @@ public class PatternMatching {
      * @return
      */
     public boolean patternMatching(String pattern, String value) {
+        int count_a = 0; int count_b = 0;
+        for (char ch:pattern.toCharArray()) {
+            if(ch == 'a'){
+                ++ count_a;
+            }else {
+                ++ count_b;
+            }
+        }
+
+        if(count_a < count_b){
+            int temp = count_a;
+            count_a = count_b;
+            count_b = temp;
+            char[] arrays = pattern.toCharArray();
+            for (int i = 0; i < arrays.length; i++) {
+                arrays[i] = arrays[i] == 'a' ?'b':'a';
+            }
+            pattern = new String(arrays);
+        }
+
+        if(value.length() == 0){
+            return count_b == 0;
+        }
+
+        if(pattern.length() ==0)
+            return false;
+
+        for (int len_a = 0; count_a * len_a <= value.length(); ++len_a) {
+            int rest = value.length() - count_a * len_a;
+            if ((count_b == 0 && rest == 0) || (count_b != 0 && rest % count_b == 0)) {
+                int len_b = (count_b == 0 ? 0 : rest / count_b);
+                int pos = 0;
+                boolean correct = true;
+                String value_a = "", value_b = "";
+                for (char ch: pattern.toCharArray()) {
+                    if (ch == 'a') {
+                        String sub = value.substring(pos, pos + len_a);
+                        if (value_a.length() == 0) {
+                            value_a = sub;
+                        } else if (!value_a.equals(sub)) {
+                            correct = false;
+                            break;
+                        }
+                        pos += len_a;
+                    } else {
+                        String sub = value.substring(pos, pos + len_b);
+                        if (value_b.length() == 0) {
+                            value_b = sub;
+                        } else if (!value_b.equals(sub)) {
+                            correct = false;
+                            break;
+                        }
+                        pos += len_b;
+                    }
+                }
+                if (correct && !value_a.equals(value_b)) {
+                    return true;
+                }
+            }
+        }
+
         return false;
     }
 }
