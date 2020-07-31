9d3228d03ab902ac3a04fb7260e72394f2f4a21c
stephenZkang <qiaok9921@163.com>
2020-07-31 08:45:13 +0800

    343. 整数拆分

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice2/IntegerBreak.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice2/IntegerBreak.java
@@ -32,12 +32,22 @@ public class IntegerBreak {
     }
 
     /**
+     * 动态规划
      * 时间复杂度：
      * 空间复杂度：
      * @param n
      * @return
      */
     public int integerBreak(int n) {
-        return 0;
+        if(n < 4){
+            return n-1;
+        }
+        int[] dp = new int[n+1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(Math.max(2 * (i - 2), 2 * dp[i - 2]), Math.max(3 * (i - 3), 3 * dp[i - 3]));
+        }
+
+        return dp[n];
     }
 }
