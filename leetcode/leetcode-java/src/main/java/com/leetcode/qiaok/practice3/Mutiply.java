4fc467c41c45e5ff0b750e46a39fc262d9961822
stephenZkang <qiaok9921@163.com>
2020-08-20 08:57:43 +0800

    43. 字符串相乘

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/Mutiply.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/Mutiply.java
@@ -26,8 +26,8 @@ public class Mutiply {
 
     public static void  main(String[] args){
         Mutiply test = new Mutiply();
-        String num1 = "";
-        String num2 = "";
+        String num1 = "123";
+        String num2 = "456";
         long start = System.currentTimeMillis();
         String res = test.mutiply(num1,num2);
         System.out.println("耗时"+(System.currentTimeMillis() - start)+"毫秒");
@@ -43,6 +43,25 @@ public class Mutiply {
      * @return
      */
     private String mutiply(String num1, String num2) {
-        return null;
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int[] res = new int[num1.length() + num2.length()];
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int n1 = num1.charAt(i) - '0';
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                int n2 = num2.charAt(j) - '0';
+                int sum = (res[i + j + 1] + n1 * n2);
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < res.length; i++) {
+            if (i == 0 && res[i] == 0) continue;
+            result.append(res[i]);
+        }
+        return result.toString();
     }
 }
