ced0d65bfb178e6f968924c94a36397d7d8a28b0
stephenZkang <qiaok9921@163.com>
2020-09-02 09:10:55 +0800

    17. 电话号码的字母组合

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/LetterCombination.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/LetterCombination.java
@@ -1,6 +1,9 @@
 package com.leetcode.qiaok.practice3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 17. 电话号码的字母组合
@@ -26,7 +29,7 @@ public class LetterCombination {
 
     public static void main(String[] args){
         LetterCombination test = new LetterCombination();
-        String digits = "";
+        String digits = "23";
         long start = System.currentTimeMillis();
         List<String> res = test.letterCombiantion(digits);
         System.out.println("耗时"+(System.currentTimeMillis() - start)+"毫秒");
@@ -41,6 +44,32 @@ public class LetterCombination {
      * @return
      */
     public List<String> letterCombiantion(String digits) {
-        return null;
+        List<String> combiantions = new ArrayList<>();
+        if(digits.length() ==0){
+            return combiantions;
+        }
+
+        Map<Character,String> phoneMap = new HashMap<Character,String>(){{
+          put('2',"abc");put('3',"def");put('4',"ghi");put('5',"jkl");
+          put('6',"mno");put('7',"pqrs");put('8',"tuv");put('9',"wxyz");
+        }};
+        backtrack(combiantions,phoneMap,digits,0,new StringBuffer());
+        return combiantions;
+    }
+
+    private void backtrack(List<String> combinations, Map<Character, String> phoneMap,
+                           String digits, int index, StringBuffer combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
     }
 }
