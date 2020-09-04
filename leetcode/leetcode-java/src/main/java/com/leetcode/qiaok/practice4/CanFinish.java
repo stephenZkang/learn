f44208cbefafc70d9f62802a475858908323ced8
stephenZkang <qiaok9921@163.com>
2020-09-04 09:31:14 +0800

    207. 课程表

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice4/CanFinish.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice4/CanFinish.java
@@ -1,5 +1,8 @@
 package com.leetcode.qiaok.practice4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 207. 课程表
  * 你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
@@ -36,7 +39,7 @@ public class CanFinish {
     public static void main(String[] args){
         CanFinish test = new CanFinish();
 
-        int numCourses = 0;
+        int numCourses = 2;
         int[][] prerequisites = {
                 {1,0},{0,1}
         };
@@ -46,7 +49,9 @@ public class CanFinish {
         System.out.println("耗时"+(System.currentTimeMillis() - start)+"毫秒");
         System.out.println("res="+res);
     }
-
+    List<List<Integer>> edges;
+    int[] visited;
+    boolean valid = true;
     /**
      * 时间复杂度：
      * 空间复杂度：
@@ -55,6 +60,37 @@ public class CanFinish {
      * @return
      */
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        return false;
+        edges = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            edges.add(new ArrayList<>());
+        }
+
+        visited = new int[numCourses];
+        for (int[] info :prerequisites) {
+            edges.get(info[1]).add(info[0]);
+        }
+
+        for (int i = 0; i < numCourses && valid; i++) {
+            if (visited[i] == 0) {
+                dfs(i);
+            }
+        }
+        return valid;
+    }
+
+    private void dfs(int u) {
+        visited[u] = 1;
+        for (int v: edges.get(u)) {
+            if (visited[v] == 0) {
+                dfs(v);
+                if (!valid) {
+                    return;
+                }
+            } else if (visited[v] == 1) {
+                valid = false;
+                return;
+            }
+        }
+        visited[u] = 2;
     }
 }
