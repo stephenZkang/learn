0c6ba78a1661f250593745d1db8a9133452a780b
stephenZkang <qiaok9921@163.com>
2020-09-08 09:19:12 +0800

    486. 预测赢家

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/PredictTheWinner.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice3/PredictTheWinner.java
@@ -55,6 +55,16 @@ public class PredictTheWinner {
      * @return
      */
     public boolean predictTheWinner(int[] nums) {
-        return false;
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = n - 2; i >= 0 ; i--) {
+            for (int j = i+1; j < n ; j++) {
+                dp[i][j] = Math.max(nums[i] - dp[i+1][j],nums[j] - dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1] > 0;
     }
 }
