38a1fd0371e9e63d37a3b07abb924a2b51bc0289
stephenZkang <qiaok9921@163.com>
2020-09-09 09:03:31 +0800

    77. 组合 回溯

--- leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice2/Combine.java
+++ leetcode/leetcode-java/src/main/java/com/leetcode/qiaok/practice2/Combine.java
@@ -1,5 +1,8 @@
 package com.leetcode.qiaok.practice2;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -43,6 +46,24 @@ public class Combine {
      * @return
      */
     public List<List<Integer>> combine(int n, int k) {
-        return null;
+        List<List<Integer>> res = new ArrayList<>();
+        if(n<=0||n<k){
+            return res;
+        }
+
+        Deque<Integer> path = new LinkedList<>();
+        dfs(n,k,1,path,res);
+        return res;
+    }
+
+    private void dfs(int n, int k, int begin, Deque<Integer> path, List<List<Integer>> res) {
+        if(path.size() == k){
+            res.add(new ArrayList<>(path));
+        }
+        for (int i = begin; i <= n; i++) {
+            path.addLast(i);
+            dfs(n,k,i+1,path,res);
+            path.removeLast();
+        }
     }
 }
